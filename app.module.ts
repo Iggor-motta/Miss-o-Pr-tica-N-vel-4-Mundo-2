8154dca32f5db1e19bb7be9ddff79061aa408f9f
Iggor-motta <132962044+Iggor-motta@users.noreply.github.com>
2024-06-01 13:05:06 -0300

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { ControleEditoraService } from './controle-editora.service';
+import { ControleLivrosService } from './controle-livros.service';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LivroListaComponent } from './livro-lista/livro-lista.component';
+import { LivroDadosComponent } from './livro-dados/livro-dados.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AppComponent, LivroListaComponent, LivroDadosComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [ControleEditoraService, ControleLivrosService],
+  bootstrap: [AppComponent],
+})
+
+export class AppModule {
+
+}
